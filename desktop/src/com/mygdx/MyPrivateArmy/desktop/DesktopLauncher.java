3e6d0d1cf2f9a70308e86532f0c43a3545278a73
Karol Daniel Skorupski <daniel.karol.skorupski@gmail.com>
2015-05-25 21:57:41 +0200

    First update

---
+++ desktop/src/com/mygdx/MyPrivateArmy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.MyPrivateArmy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.MyPrivateArmy.MyPrivateArmy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyPrivateArmy(), config);
+	}
+}
