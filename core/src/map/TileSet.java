2835833159c60bcb4aadaa6bb4bbf99614dce1c2
Karol Daniel Skorupski <daniel.karol.skorupski@gmail.com>
2015-06-05 19:48:28 +0200

    Revert "Revert "Update - running state""
    This reverts commit dbf68dfe6ed97f70cf81b0e31e69df7d0bf85c16.

--- core/src/map/TileSet.java
+++
@@ -1,7 +0,0 @@
-package map;
-
-public class TileSet {
-	public TileSet(){
-		
-	}
-}
--- core/src/stats/Running.java
+++ core/src/stats/Running.java
@@ -1,24 +1,50 @@
 package stats;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.FPSLogger;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.MyPrivateArmy.MyPrivateArmy;
 
 public class Running implements Screen {
 
-    MyPrivateArmy mainGame;
+    private MyPrivateArmy mainGame;
+    private OrthographicCamera camera;
     
-     public Running(MyPrivateArmy mainGame){
+    private SpriteBatch batch;
+    private FPSLogger fpsLogger;
+    private BitmapFont font;
+    
+    public Running(MyPrivateArmy mainGame){
              this.mainGame = mainGame;
     }
      
 	@Override
 	public void show() {
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera();
+		fpsLogger = new FPSLogger();
+		font = new BitmapFont();
 		
+		camera.setToOrtho(false, 800, 600);
 	}
 
 	@Override
 	public void render(float delta) {
-		
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        font.draw(batch, "NO ELO", 100, 150);
+        batch.end();
+        
+        fpsLogger.log();
 	}
 
 	@Override
